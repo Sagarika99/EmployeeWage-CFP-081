e4d63197b7ae0c8170edd49b443a9484619161e9
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-09 17:17:05 +0530

    [sagarika]: Employee Wage for a month calculated

--- src/com/bridgelabz/employeewage/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComputation.java
@@ -6,24 +6,31 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		int EmpWagePerHr = 20;
-		int EmpWage;
+		int EmpWorkingDays = 20;
+		int EmpWage=0;
 		int EmpHrs = 0;
+		int EmpTotalWage = 0;
 		
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		for (int i=0 ; i<=EmpWorkingDays; i++) {
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+				case is_full_time :
+					EmpHrs = 8;
+					break;
+				case is_part_time :
+					EmpHrs = 4;
+					break;
+				default :
+					EmpHrs = 0;
+			}
+			EmpWage = EmpHrs * EmpWagePerHr;
+			EmpTotalWage += EmpWage;
+			System.out.println("Emp wage :" +EmpWage);
+		}		
+		System.out.println("Employee Total wage is " +EmpTotalWage);
 		
-		switch (empCheck) {
-		case is_full_time :
-			EmpHrs = 8;
-			break;
-		case is_part_time :
-			EmpHrs = 4;
-			break;
-		default :
-			EmpHrs = 0;
-		}
 
-		EmpWage = EmpHrs * EmpWagePerHr;
-		System.out.println("Employee Daily wage is " +EmpWage);
+		
 
 	}
 
