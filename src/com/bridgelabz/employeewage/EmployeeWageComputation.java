f57239a6b0d987492557978447daf0ad29a18305
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-09 14:09:25 +0530

    [sagarika] : Added Employee attendance

--- src/com/bridgelabz/employeewage/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComputation.java
@@ -3,7 +3,13 @@ package com.bridgelabz.employeewage;
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int is_present = 1;
+		
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == is_present)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 
 	}
 
