4eabb450325236a941b7c64326ff819be24e75fd
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-09 16:38:48 +0530

    [sagarika]: Solved using Switch case statement

--- src/com/bridgelabz/employeewage/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComputation.java
@@ -1,22 +1,27 @@
 package com.bridgelabz.employeewage;
 
 public class EmployeeWageComputation {
+	public static final int is_full_time = 1;
+	public static final int is_part_time = 2;
 
 	public static void main(String[] args) {
-		int is_present = 1;
-		int is_part_time = 2;
 		int EmpWagePerHr = 20;
 		int EmpWage;
 		int EmpHrs = 0;
 		
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (empCheck == is_present)
-			EmpHrs = 8;
-		else if (empCheck == is_part_time)
-			EmpHrs = 4;
-		else
-			System.out.println("Employee is Absent");
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		
+		switch (empCheck) {
+		case is_full_time :
+			EmpHrs = 8;
+			break;
+		case is_part_time :
+			EmpHrs = 4;
+			break;
+		default :
+			EmpHrs = 0;
+		}
+
 		EmpWage = EmpHrs * EmpWagePerHr;
 		System.out.println("Employee Daily wage is " +EmpWage);
 
