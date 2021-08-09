f1daf2aa0effdc044239642f340cc81b5f6e0c6f
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-09 16:06:57 +0530

    [sagarika]: EMployee daily wage calculated

--- src/com/bridgelabz/employeewage/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComputation.java
@@ -4,12 +4,18 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		int is_present = 1;
+		int EmpWagePerHr = 20;
+		int EmpWage;
+		int EmpHrs = 0;
 		
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == is_present)
-			System.out.println("Employee is Present");
+			EmpHrs = 8;
 		else
 			System.out.println("Employee is Absent");
+		
+		EmpWage = EmpHrs * EmpWagePerHr;
+		System.out.println("Employee Daily wage is " +EmpWage);
 
 	}
 
