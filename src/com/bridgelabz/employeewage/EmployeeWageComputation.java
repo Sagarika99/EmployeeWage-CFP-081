f479b3e6a7804e39a891f40f205f6a52afd5bcc8
Sagarika Shinde <sagarikashinde99@gmail.com>
2021-08-09 16:17:05 +0530

    [sagarika]: EMployee part time and full time wage calculated

--- src/com/bridgelabz/employeewage/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComputation.java
@@ -4,13 +4,16 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		int is_present = 1;
+		int is_part_time = 2;
 		int EmpWagePerHr = 20;
 		int EmpWage;
 		int EmpHrs = 0;
 		
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		if (empCheck == is_present)
 			EmpHrs = 8;
+		else if (empCheck == is_part_time)
+			EmpHrs = 4;
 		else
 			System.out.println("Employee is Absent");
 		
